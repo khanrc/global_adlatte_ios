9bd57e5be9a492c2482ef8eaf022b8376cf53408
Taehan Kim <taehan@adlatte.com>
2013-02-04 16:57:57 +0900

    Initial Commit

---
+++ adlatte/ADSecondViewController.m
@@ -0,0 +1,39 @@
+//
+//  ADSecondViewController.m
+//  adlatte
+//
+//  Created by Tae Han Kim on 13. 2. 4..
+//  Copyright (c) 2013년 Tae Han Kim. All rights reserved.
+//
+
+#import "ADSecondViewController.h"
+
+@interface ADSecondViewController ()
+
+@end
+
+@implementation ADSecondViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = NSLocalizedString(@"Second", @"Second");
+        self.tabBarItem.image = [UIImage imageNamed:@"second"];
+    }
+    return self;
+}
+							
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ adlatte/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  adlatte
+//
+//  Created by Tae Han Kim on 13. 2. 4..
+//  Copyright (c) 2013년 Tae Han Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ADAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ADAppDelegate class]));
+    }
+}
